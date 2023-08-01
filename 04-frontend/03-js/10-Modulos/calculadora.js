1aaa6269715aa5f6a9c8b2072c11fd2886da8ebd
Dairon <daironpotes73@gmail.com>
2023-08-01 17:45:23 -0500

    DOM y Modulos Pagina barra lateral

---
+++ 04-frontend/03-js/10-Modulos/calculadora.js
@@ -0,0 +1,7 @@
+export function sumar(a, b) {
+  console.log(a + b);
+}
+
+export function saludar() {
+  console.log("Hola soy manolo");
+}
---
+++ 04-frontend/03-js/10-Modulos/index.js
@@ -0,0 +1,3 @@
+import { sumar, saludar } from "./calculadora.js";
+sumar(10, 20);
+saludar();
